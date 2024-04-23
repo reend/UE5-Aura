c9a0f1522015014ade8521fad86377d2ea75b749
Kyrylo Prylipko <kyrylo.pr@oxoix.com>
2024-04-23 19:55:20 +0200

    Init Ability Actor Info

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+#include <Player/AuraPlayerState.h>
 
 AAuraCharacter::AAuraCharacter()
 {
@@ -13,4 +15,25 @@ AAuraCharacter::AAuraCharacter()
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
+}
+
+void AAuraCharacter::PossessedBy(AController* NewController)
+{
+	Super::PossessedBy(NewController);
+	InitiAbilityActorInfo();
+}
+
+void AAuraCharacter::OnRep_PlayerState()
+{
+	Super::OnRep_PlayerState();
+	InitiAbilityActorInfo();
+}
+
+void AAuraCharacter::InitiAbilityActorInfo()
+{
+	AAuraPlayerState* AuraPlayerState = GetPlayerState<AAuraPlayerState>();
+	check(AuraPlayerState);
+	AuraPlayerState->GetAbilitySystemComponent()->InitAbilityActorInfo(AuraPlayerState, this);
+	AbilitySystemComponent = AuraPlayerState->GetAbilitySystemComponent();
+	AttributeSet = AuraPlayerState->GetAttributeSet();
 }
