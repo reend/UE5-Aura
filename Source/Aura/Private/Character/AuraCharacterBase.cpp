e14eb4921f708d551c5bf072699a989626932a9e
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2025-03-24 21:07:37 +0100

    c6
    init classes: Base, Character, Enemy

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,18 @@
+
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+// Called when the game starts or when spawned
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
