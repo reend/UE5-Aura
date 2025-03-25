cbb38d1933448c18864cc033f09f7e27e5837bc0
Kyrylo Prylipko <24914627+reend@users.noreply.github.com>
2025-03-25 21:01:17 +0100

    Movement Input

--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -7,7 +7,8 @@
 #include "AuraPlayerController.generated.h"
 
 class UInputMappingContext;
-
+class UInputAction;
+struct FInputActionValue;
 /**
  * 
  */
@@ -20,7 +21,13 @@ public:
 	AAuraPlayerController();
 protected:
 	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
 private:
 	UPROPERTY(EditAnywhere, Category = "Input")
 	TObjectPtr<UInputMappingContext> AuraContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	TObjectPtr<UInputAction> MoveAction;
+
+	void Move(const FInputActionValue& InputActionValue);
 };
